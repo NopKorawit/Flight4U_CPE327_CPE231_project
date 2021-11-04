31395c61f8ecc21460d350244c060abedb94c6bd
NopKorawit <korawit.was@gmail.com>
2021-11-04 23:17:32 +0700

    init

---
+++ flight_booking/urls.py
@@ -0,0 +1,16 @@
+from typing import ValuesView
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("my_booking",views.my_booking),
+    path("searchyourflight",views.search),
+    path("register",views.registerForm),
+    path("viewflight",views.viewflight),
+    path("addUser",views.addUser),
+    path("loginform",views.loginform),
+    path("login",views.login),
+    
+
+]
